c278500162e01d8b979e30a3183de67c77e2ed9f
Abdullahbdbd <121636151+Abdullahbdbd@users.noreply.github.com>
2026-06-23 20:28:31 +0600

    create query 4

--- query.sql
+++ query.sql
@@ -25,3 +25,13 @@ SELECT
 FROM Bookings
 WHERE payment_status IS NULL;
 
+-- query 4
+SELECT 
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    round(b.total_cost, 0)
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id;
+
